21ceb4a38efc88f517b7dbee5f33fe89f8fc28cb
Milan <milan.kljajic92@gmail.com>
2026-01-29 18:33:14 +0100

    Created entity classes

---
+++ BusStopManagement.Core/Domain/Entities/BusStop.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusStopManagement.Core.Domain.Entities
+{
+    public class BusStop
+    {
+        [Key]
+        public Guid BusStopID { get; set; }
+
+        public ICollection<Departure>? Departures { get; set; } = [];
+    }
+}
---
+++ BusStopManagement.Core/Domain/Entities/Departure.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusStopManagement.Core.Domain.Entities
+{
+    public class Departure
+    {
+        [Key]
+        public Guid DepartureID { get; set; }
+        
+        public string Destination { get; set; } = string.Empty;
+        
+        public DateTime DateAndTimeOfDeparture { get; set; }
+        
+        public byte NumberOfSeats { get; set; }
+
+        public Guid BusStopID { get; set; }
+
+        [ForeignKey("BusStopID")]
+        public BusStop BusStop { get; set; } = null!;
+    }
+}
